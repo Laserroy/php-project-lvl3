37a505f76a20b51e3691bfc1772170bdafec91b1
laserX <deectro@gmail.com>
2019-11-07 11:14:03 +0100

    add linter

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -15,7 +15,7 @@ class ExampleTest extends TestCase
         $this->get('/');
 
         $this->assertEquals(
-            $this->app->version(), $this->response->getContent()
+            $this->app->version(), 'Lumen (6.2.0) (Laravel Components ^6.0)'
         );
     }
 }
